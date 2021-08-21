ede70fbf7702c8e28d920c82936a277ec3bc3f78
Ivan Strizhko <62095188+dante89919001@users.noreply.github.com>
2021-08-21 20:18:06 +0600

    Update index.js
    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,50 @@
 
-exports.min = function min (array) {
-  return 0;
+exports.min = function min (array =[undefined]) {
+  if(array[0]=== undefined)
+  {
+    return 0;
+  }
+  let Minimum = array[0];
+  for ( var i = 0 ; i < array.length; i++)
+  {
+    if(array[i] < Minimum)
+    {
+      Minimum = array[i];
+    }
+  }
+  return Minimum;
 }
 
-exports.max = function max (array) {
-  return 0;
+exports.max = function max (array =[undefined]) {
+  if(array[0]=== undefined)
+  {
+    return 0;
+  }
+  let Maximum = array[0];
+  for ( var i = 0 ; i < array.length; i++)
+  {
+    if(array[i] > Maximum)
+    {
+      Maximum = array[i];
+    }
+  }
+  return Maximum;
 }
 
-exports.avg = function avg (array) {
-  return 0;
+exports.avg = function avg (array =[undefined]) {
+  if(array[0]=== undefined)
+  {
+    return 0;
+  }
+  let i = 0;
+  let result = 0;
+  while(i < array.length )
+  {
+      result += array[i];
+    i++;
+  }
+  
+  result = result/array.length;
+
+  return result;
 }
